ba51c032282dd032b94051b844900d549c82636e
Jeevan Shriram <jshriram@codeaurora.org>
2014-04-28 18:04:07 -0700

    mdss: fb: Add backward compatibility to fb memory
    In the current implementation, contiguous memory allocation
    during bootup is removed and related changes were made in
    frame buffer driver which is not backward compatible. With this
    change, add backward compatibility to map to physically contiguous
    memory when available which is allocated at bootup.
    
    Change-Id: Ib1a595f2426dda8d3e1c5e50c71c35d3f45854b6
    Signed-off-by: Jeevan Shriram <jshriram@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_fb.h

--- drivers/video/msm/mdss/mdss_fb.h
+++ drivers/video/msm/mdss/mdss_fb.h
@@ -262,12 +262,9 @@ void mdss_fb_signal_timeline(struct msm_sync_pt_data *sync_pt_data);
 struct sync_fence *mdss_fb_sync_get_fence(struct sw_sync_timeline *timeline,
 				const char *fence_name, int val);
 int mdss_fb_register_mdp_instance(struct msm_mdp_interface *mdp);
-<<<<<<< HEAD
-=======
 #if defined(CONFIG_MDNIE_TFT_MSM8X26)
 void mdss_negative_color(int is_negative_on);
 #endif
-int mdss_fb_alloc_fb_ion_memory(struct msm_fb_data_type *mfd);
->>>>>>> feaa12e... mdss: msm_fb: Allocate fb memory during mmap
 int mdss_fb_dcm(struct msm_fb_data_type *mfd, int req_state);
+int mdss_fb_suspres_panel(struct device *dev, void *data);
 #endif /* MDSS_FB_H */
